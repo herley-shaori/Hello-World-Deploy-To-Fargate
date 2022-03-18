0f99536ac63a6077f72ed8c179b4fbe5de8dd46c
herley <herley.shaori@gmail.com>
2022-03-18 17:49:18 +0700

    initial commit.

---
+++ src/test/java/com/example/simple/hello/SimpleHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
