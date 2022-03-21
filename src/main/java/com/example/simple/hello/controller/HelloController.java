ef41e328578cd7af1232c538a2334cc3c646bbe1
herley <herley.shaori@gmail.com>
2022-03-21 16:30:44 +0700

    return to hello world.

--- src/main/java/com/example/simple/hello/controller/HelloController.java
+++ src/main/java/com/example/simple/hello/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String getHelloWorldResponse(){
-        return "Hello World. AWS Fargate Berhasil. Ini dari Hello-Pipeline-2.";
+        return "Hello World.";
     }
 }
