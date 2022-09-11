03c517c0c53a6c1ca12638f11459806a63be8d57
herley <herley.shaori@gmail.com>
2022-09-11 19:01:54 +0700

    v4.

--- src/main/java/com/example/simple/hello/controller/HelloController.java
+++ src/main/java/com/example/simple/hello/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String getHelloWorldResponse(){
-        return "Hello World v3.";
+        return "Hello World v4.";
     }
 }
